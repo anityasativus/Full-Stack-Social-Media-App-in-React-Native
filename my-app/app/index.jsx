52df801cdf05a2849456fe6a308b7fe6bb8df74a
Anitya Barua <137085120+anityasativus@users.noreply.github.com>
2025-03-20 22:30:18 +0600

    font

--- my-app/app/index.jsx
+++ my-app/app/index.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useRouter } from 'expo-router'
 
 const index = () => {
+  const router =useRouter();
   return (
     <View>
       <Text>index</Text>
+      <Button title="welcome" onPress={()=> router.push('welcome')} />
     </View>
   )
 }
